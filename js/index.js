6af7ce12bacf29669eda7beaacebf6bbf8c4fe21
ecomeel <ecomeel@gmail.com>
2024-01-28 18:37:16 +0300

    DOM & Storage

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
-function count(obj) {
-  return Object.keys(obj).length
-}
+// let x = null;
+// x ??= false;
+// x ??= 20;
+// x ??= underfind;
+// console.log(x)
 
-let user = {
-  name: 'John',
-  age: 30
-};
+// var x = 10;
+// function bar(fun) {
+//   var x = 30;
+//   fun()
+// }
+// function foo() {
+//   console.log(x)
+// }
+// foo.x = 20;
+// bar.x = 40;
+// bar(foo)
 
-console.log(count(user));
+
+// const person1 = {
+//   name: 'Sasha',
+//   prof: 'web-dev',
+//   hi() {console.log('Hello, my name is', this.name)}
+// }
+// const person2 = {
+//   name: 'Petr'
+// }
+// person1.hi();
+// person1.hi.call(person2)
+// person1.hi.apply(person2)
+// person2.hi = person1.hi.bind(this)
+// person2.hi()
+// person1.hi()
+
